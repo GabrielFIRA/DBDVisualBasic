e8387c7b19115eb0aadaf2c40ce11ee418e2a6d6
Gabriel Mayorga <gabrielmayorgate@gmail.com>
2022-05-29 22:17:55 -0600

    0.12

---
+++ frmRptDepartamentos.Designer.vb
@@ -0,0 +1,91 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmRptDepartamentos
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ReportDataSource6 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.biomessDataSet2 = New Biomess_0._1.biomessDataSet2()
+        Me.DepartamentoBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.DepartamentoTableAdapter = New Biomess_0._1.biomessDataSet2TableAdapters.DepartamentoTableAdapter()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.biomessDataSet2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DepartamentoBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ReportViewer1
+        '
+        ReportDataSource6.Name = "dsInformes"
+        ReportDataSource6.Value = Me.DepartamentoBindingSource
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource6)
+        Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "Biomess_0._1.rptDepartamentos.rdlc"
+        Me.ReportViewer1.Location = New System.Drawing.Point(1, 0)
+        Me.ReportViewer1.Name = "ReportViewer1"
+        Me.ReportViewer1.ServerReport.BearerToken = Nothing
+        Me.ReportViewer1.Size = New System.Drawing.Size(801, 452)
+        Me.ReportViewer1.TabIndex = 1
+        '
+        'biomessDataSet2
+        '
+        Me.biomessDataSet2.DataSetName = "biomessDataSet2"
+        Me.biomessDataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'DepartamentoBindingSource
+        '
+        Me.DepartamentoBindingSource.DataMember = "Departamento"
+        Me.DepartamentoBindingSource.DataSource = Me.biomessDataSet2
+        '
+        'DepartamentoTableAdapter
+        '
+        Me.DepartamentoTableAdapter.ClearBeforeFill = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(727, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Atras"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'frmRptDepartamentos
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ReportViewer1)
+        Me.Name = "frmRptDepartamentos"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Reporte Departamento"
+        CType(Me.biomessDataSet2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DepartamentoBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
+    Friend WithEvents DepartamentoBindingSource As BindingSource
+    Friend WithEvents biomessDataSet2 As biomessDataSet2
+    Friend WithEvents DepartamentoTableAdapter As biomessDataSet2TableAdapters.DepartamentoTableAdapter
+    Friend WithEvents Button1 As Button
+End Class
---
+++ frmRptDepartamentos.vb
@@ -0,0 +1,14 @@
+﻿Public Class frmRptDepartamentos
+    Private Sub frmRptDepartamentos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: esta línea de código carga datos en la tabla 'biomessDataSet2.Departamento' Puede moverla o quitarla según sea necesario.
+        Me.DepartamentoTableAdapter.Fill(Me.biomessDataSet2.Departamento)
+
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim win = New frmPantallaEmpresa()
+        win.Show()
+        Me.Dispose()
+    End Sub
+End Class
